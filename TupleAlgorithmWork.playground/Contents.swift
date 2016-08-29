d00dafa224d0b6c49db40d8430dec1550b6c87b3
Rachel Parsons <vc.rachelanne.parsons@lowes.com>
2016-08-29 08:44:07 -0400

    updated with latest algorithm work

--- TupleAlgorithmWork.playground/Contents.swift
+++ TupleAlgorithmWork.playground/Contents.swift
@@ -30,8 +30,30 @@ func updateOption(forRow row: Int, withNewValue newValue: Int) {
     } else {
         selectedOptions.append(newSelection)
     }
+    
+    
+}
 
-
+func findSelectionForRow(row: Int) -> Int? {
+    
+    var tupleFound = false
+    var selectedIndex = 0
+    
+    for tuple in selectedOptions {
+        switch tuple {
+        case (row, let selectedValue) where row == tuple.variantIndex:
+            selectedIndex = selectedValue
+            tupleFound = true
+        default:
+            break
+        }
+    }
+    
+    if tupleFound {
+        return selectedIndex
+    } else {
+        return nil
+    }
 }
 
 updateOption(forRow: 3, withNewValue: 123)
@@ -40,8 +62,18 @@ print(selectedOptions)
 updateOption(forRow: 1, withNewValue: 87)
 print(selectedOptions)
 
+let test1 = findSelectionForRow(1)
+print(test1)
+
 updateOption(forRow: 3, withNewValue: 999)
 print(selectedOptions)
 
 updateOption(forRow: 1, withNewValue: 866667)
 print(selectedOptions)
+
+let test2 = findSelectionForRow(3)
+
+updateOption(forRow: 6, withNewValue: 2)
+print(selectedOptions)
+
+//pull selected index
